fb272cfd0a1048a698c2bdfb687f8714131af900
naman79820 <naman79820@gmail.com>
2026-02-09 15:03:52 +0530

    chore: added simulator.vue

--- src/pages/simulator.vue
+++ src/pages/simulator.vue
@@ -1,8 +1,14 @@
 <template>
-    <Navbar />
-    <ContextMenu />
-    <Extra />
-    <Helper />
+    <div class="simulator-container">
+        <Navbar />
+        <DebugControls />
+        <ContextMenu />
+        <Extra />
+        <Helper />
+        <DebugTimeline />
+        <!-- Add Breakpoint Panel -->
+        <BreakpointPanel />
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -12,12 +18,18 @@ import Extra from '@/Extra.vue'
 import { defineComponent, onMounted } from 'vue'
 import { setup as setupSimulator } from '../simulator/src/setup'
 import Helper from '#/components/helpers/Helper.vue'
+import DebugControls from '#/components/DebugControls.vue'
+import DebugTimeline from '#/components/DebugTimeline.vue'
+import BreakpointPanel from '#/components/BreakpointPanel.vue'
 
 defineComponent({
     components: {
         Navbar,
         ContextMenu,
         Extra,
+        DebugControls,
+        DebugTimeline,
+        BreakpointPanel,
     },
 })
 
@@ -25,3 +37,13 @@ onMounted(() => {
     setupSimulator()
 })
 </script>
+
+<style scoped>
+.simulator-container {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+}
+</style>
